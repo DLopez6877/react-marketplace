44aa68552477908a2e956ca11e9c966c2195edcf
Daniel Lopez <dlopez6877@gmail.com>
2017-11-20 16:49:06 -0800

    setup marketplace with passing one prop for catch of the day

---
+++ catch-of-the-day/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import './css/style.css';
+import AppComponent from './components/App';
+
+render(<AppComponent/>, document.querySelector('#main'));
