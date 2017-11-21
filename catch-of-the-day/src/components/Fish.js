7b35dbd842150be80fbebbcb4e3e74c5fea4e3dc
Daniel Lopez <dlopez6877@gmail.com>
2017-11-20 23:10:42 -0800

    add dynamic addToOrder functoitionality to app

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -3,9 +3,11 @@ import { formatPrice } from '../helpers';
 
 class Fish extends React.Component {
     render() {
+        const { details, index } = this.props;
+        // const details = this.props.details; //without es6 deconstructuring
+        const isAvailable = details.status === "available";
+        const buttonText = isAvailable ? 'Add to Order' : 'Sold Out!';
 
-        const { details } = this.props;
-        // const details = this.props.details; //without deconstructuring
         return (
             <li className="menu-fish">
                 <img src={details.image} alt={details.name} />
@@ -14,7 +16,7 @@ class Fish extends React.Component {
                     <span className="price">{formatPrice(details.price)}</span>
                 </h3>
                 <p>{details.desc}</p>
-                <button>Add To Order</button>
+                <button disabled={!isAvailable} onClick={() => this.props.addToOrder(index)}>{buttonText}</button>
             </li>
         )
     }
