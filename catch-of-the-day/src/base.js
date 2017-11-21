56d53d9408cad37596ebf10b674d9370866b432b
Daniel Lopez <dlopez6877@gmail.com>
2017-11-21 13:14:08 -0800

    link store and fishes to firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyCX3rzMpq66H1rzAjr5r6319HJ-9mL6mRA",
+    authDomain: "catch-of-the-day-djl.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-djl.firebaseio.com"
+});
+
+export default base;
