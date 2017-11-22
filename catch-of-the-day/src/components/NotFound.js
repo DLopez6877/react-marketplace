0906cd2fbeaef2bd46850a31b3790717de6c2b82
Daniel Lopez <dlopez6877@gmail.com>
2017-11-21 19:44:44 -0800

    add downasaur

--- catch-of-the-day/src/components/NotFound.js
+++ catch-of-the-day/src/components/NotFound.js
@@ -3,7 +3,10 @@ import React from 'react';
 class NotFound extends React.Component {
     render() {
         return (
-            <p>NOtFound works!</p>
+            <div className="404">
+                <div className="downasaur"></div>
+                <h1>404 PAGE NOT FOUND</h1>
+            </div>
         )
     }
 }
